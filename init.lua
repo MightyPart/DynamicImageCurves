04963ed5753616e3798cf2a18baa7675b25a03ef
MightyPart <cameron.campbell325@gmail.com>
2023-09-18 21:21:50 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 local Image = {}; Image.__index = Image
 
-
 --> Helper Functions -----------------------------------------------------------------------------
 local function CreatePixels(amount, r,g,b, alpha)
 	local pixels = table.create(amount * 4)
@@ -14,6 +13,11 @@ local function CreatePixels(amount, r,g,b, alpha)
 end
 
 local function Lerp(a, b, t) return a + (b-a) * t end
+
+local function ForceToDecimal(number)
+	if number >= 0 and number <= 1 then return number end
+	return number / 100
+end
 --------------------------------------------------------------------------------------------------
 
 
@@ -23,7 +27,9 @@ function Image:Resize(size:Vector2)
 end
 
 function Image:WritePixels(position: Vector2, size: Vector2, pixels: {any})
-	return self.dynamicImage:WritePixels(position, size, pixels)
+	pcall(function()
+		return self.dynamicImage:WritePixels(position, size, pixels)
+	end)
 end
 
 function Image:DrawImage(position:Vector2, image: DynamicImage, combineType: Enum.ImageCombineType)
@@ -44,142 +50,152 @@ end
 --------------------------------------------------------------------------------------------------
 
 
-function Image:Line(p0, p1, spacingInterval, indicatorSize)
+function Image:Line(p0, p1, r,g,b,alpha, indicatorSize)
 	local p0X, p0Y = p0[1], p0[2]
 	local p1X, p1Y = p1[1], p1[2]
 
-	local sizeX, sizeY = self.sizeX, self.sizeY
+	local dist = math.round(math.abs(math.sqrt( math.pow(p1X - p0X, 2) + math.pow(p1Y - p0Y, 2) )))
 
-	for t = 0, 1, spacingInterval do
+	for t = 0, 1, 1/(dist*2) do
 		local tX = Lerp( p0X, p1X, t )
 		local tY = Lerp( p0Y, p1Y, t )
 
-		for x = 0, sizeX do
+		for x = p0X, p1X do
 			if math.abs(tX - x) >= 2 then continue end
-			for y = 0, sizeY do
+			for y = p0Y, p1Y do
 				if math.abs(tY - y) >= 2 then continue end
-				self:WritePixels(Vector2.new(x,y), Vector2.new(1,1), { 0,0,0,1 })
+				self:WritePixels(Vector2.new(x,y), Vector2.new(1,1), { r,g,b,alpha })
 			end
 		end
 	end
 
-	local halfIndicatorSize = indicatorSize/2
+	if indicatorSize and indicatorSize >= 1 then
+		local halfIndicatorSize = indicatorSize/2
+		local sizeX, sizeY = self.sizeX, self.sizeY
 
-	-- p0
-	self:WritePixels(
-		Vector2.new(math.clamp(p0X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p0Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,1,0,1)
-	)
+		-- p0
+		self:WritePixels(
+			Vector2.new(math.clamp(p0X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p0Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,1,0,1)
+		)
 
-	-- p1
-	self:WritePixels(
-		Vector2.new(math.clamp(p1X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p1Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
-	)
+		-- p1
+		self:WritePixels(
+			Vector2.new(math.clamp(p1X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p1Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
+		)
+	end
 end
 
-function Image:QuadraticBezier(p0, p1, p2, spacingInterval, indicatorSize)
+
+function Image:QuadraticBezier(p0, p1, p2, indicatorSize)
 	local p0X, p0Y = p0[1], p0[2]
 	local p1X, p1Y = p1[1], p1[2]
 	local p2X, p2Y = p2[1], p2[2]
 
-	local sizeX, sizeY = self.sizeX, self.sizeY
+	local dist = math.round(math.abs(math.sqrt( math.pow(p2X - p0X, 2) + math.pow(p2Y - p0Y, 2) )))
 
-	for t = 0, 1, spacingInterval do
+	for t = 0, 1, 1/(dist*2) do
 		local tX = Lerp( Lerp(p0X, p1X, t), Lerp(p1X, p2X, t), t )
 		local tY = Lerp( Lerp(p0Y, p1Y, t), Lerp(p1Y, p2Y, t), t )
 
-		for x = 0, sizeX do
+		for x = p0X, p2X do
 			if math.abs(tX - x) >= 2 then continue end
-			for y = 0, sizeY do
+			for y = p0Y, p2Y do
 				if math.abs(tY - y) >= 2 then continue end
 				self:WritePixels(Vector2.new(x,y), Vector2.new(1,1), { 0,0,0,1 })
 			end
 		end
 	end
 
-	local halfIndicatorSize = indicatorSize/2
+	if indicatorSize and indicatorSize >= 1 then
+		local halfIndicatorSize = indicatorSize/2
+		local sizeX, sizeY = self.sizeX, self.sizeY
 
-	-- p0
-	self:WritePixels(
-		Vector2.new(math.clamp(p0X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p0Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,1,0,1)
-	)
-	
-	-- p1
-	self:WritePixels(
-		Vector2.new(math.clamp(p1X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p1Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
-	)
+		-- p0
+		self:WritePixels(
+			Vector2.new(math.clamp(p0X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p0Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,1,0,1)
+		)
 
-	-- p2
-	self:WritePixels(
-		Vector2.new(math.clamp(p2X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p2Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 1,0,0,1)
-	)
+		-- p1
+		self:WritePixels(
+			Vector2.new(math.clamp(p1X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p1Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
+		)
+
+		-- p2
+		self:WritePixels(
+			Vector2.new(math.clamp(p2X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p2Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 1,0,0,1)
+		)
+	end
 end
 
 
-function Image:CubicBezier(p0, p1, p2, p3, spacingInterval, indicatorSize)
+function Image:CubicBezier(p0, p1, p2, p3, indicatorSize)
 	local p0X, p0Y = p0[1], p0[2]
 	local p1X, p1Y = p1[1], p1[2]
 	local p2X, p2Y = p2[1], p2[2]
 	local p3X, p3Y = p3[1], p3[2]
-	
-	local sizeX, sizeY = self.sizeX, self.sizeY
-	
-	for t = 0, 1, spacingInterval do
+
+	local dist = math.round(math.abs(math.sqrt( math.pow(p3X - p0X, 2) + math.pow(p3Y - p0Y, 2) )))
+
+	for t = 0, 1, 1/(dist*2) do
 		local l1X, l2X, l3X = Lerp(p0X, p1X, t), Lerp(p1X, p2X, t), Lerp(p2X, p3X, t)
 		local lAX, lBX = Lerp(l1X, l2X, t), Lerp(l2X, l3X, t)
 		local lCX = Lerp(lAX, lBX, t)
-		
+
 		local l1Y, l2Y, l3Y = Lerp(p0Y, p1Y, t), Lerp(p1Y, p2Y, t), Lerp(p2Y, p3Y, t)
 		local lAY, lBY = Lerp(l1Y, l2Y, t), Lerp(l2Y, l3Y, t)
 		local lCY = Lerp(lAY, lBY, t)
-		
-		for x = 0, sizeX do
-			if math.abs(lCX - x) >= 2 then continue end
-			for y = 0, sizeY do
-				if math.abs(lCY - y) >= 2 then continue end
+
+		for x = p0X, p3X do
+			if math.abs(math.round(lCX - x)) >= 3 then continue end
+			for y = p0Y, p3Y do
+				if math.abs(math.round(lCY - y)) >= 3 then continue end
 				self:WritePixels(Vector2.new(x,y), Vector2.new(1,1), { 0,0,0,1 })
 			end
 		end
 	end
-	
-	local halfIndicatorSize = indicatorSize/2
-	
-	-- p0
-	self:WritePixels(
-		Vector2.new(math.clamp(p0X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p0Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,1,0,1)
-	)
-	
-	-- p1
-	self:WritePixels(
-		Vector2.new(math.clamp(p1X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p1Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
-	)
-	
-	-- p2
-	self:WritePixels(
-		Vector2.new(math.clamp(p2X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p2Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
-	)
-	
-	-- p3
-	self:WritePixels(
-		Vector2.new(math.clamp(p3X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p3Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
-		Vector2.new(indicatorSize,indicatorSize),
-		CreatePixels(indicatorSize*indicatorSize, 1,0,0,1)
-	)
+
+	if indicatorSize and indicatorSize >= 1 then
+		local halfIndicatorSize = indicatorSize/2
+		local sizeX, sizeY = self.sizeX, self.sizeY
+
+		-- p0
+		self:WritePixels(
+			Vector2.new(math.clamp(p0X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p0Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,1,0,1)
+		)
+
+		-- p1
+		self:WritePixels(
+			Vector2.new(math.clamp(p1X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p1Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
+		)
+
+		-- p2
+		self:WritePixels(
+			Vector2.new(math.clamp(p2X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p2Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 0,0,1,1)
+		)
+
+		-- p3
+		self:WritePixels(
+			Vector2.new(math.clamp(p3X-(indicatorSize/2), 0, sizeX-indicatorSize), math.clamp(p3Y-(indicatorSize/2), 0, sizeY-indicatorSize)),
+			Vector2.new(indicatorSize,indicatorSize),
+			CreatePixels(indicatorSize*indicatorSize, 1,0,0,1)
+		)
+	end
 end
 
 
@@ -188,6 +204,7 @@ function Image:InstantiateAsLabel()
 	--label.BackgroundTransparency = 1
 	label.BackgroundColor3 = Color3.new(1,1,1)
 	label.Size = UDim2.fromOffset(self.sizeX, self.sizeY)
+	label.BorderSizePixel = 0
 	self.dynamicImage.Parent = label
 	return label
 end
@@ -198,12 +215,14 @@ function Image:Clear()
 	self:WritePixels(Vector2.new(0,0), Vector2.new(self.sizeX, self.sizeY), CreatePixels(sizeX*sizeY, 1,1,1,1))
 end
 
-
 return {
 	new = function(sizeX:number, sizeY: number)
+		assert(sizeX <= 1024, "sizeX must be equal to or less than 1024")
+		assert(sizeY <= 1024, "sizeY must be equal to or less than 1024")
+
 		local dynamicImage = Instance.new("DynamicImage")
 		dynamicImage.Size = Vector2.new(sizeX, sizeY)
-		
+
 		return setmetatable({
 			dynamicImage = dynamicImage,
 			sizeX = sizeX, sizeY = sizeY
